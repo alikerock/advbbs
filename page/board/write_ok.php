704bb8fb9a3ad060c6598679b76bf1e7cd9ac70c
rock's ezweb <alikerock@gmail.com>
2024-02-15 10:23:03 +0900

    write ok

--- page/board/write_ok.php
+++ page/board/write_ok.php
@@ -7,5 +7,15 @@
   $content = $_POST['content'];
   $date = date('Y-m-d');
   
-  $sql = //board 테이블에 저장하는 sql 문장
+  $sql = "INSERT INTO board (name,pw,title,content,date) values ('{$username}','{$userpw}','{$title}','{$content}','{$date}')";
+
+  if($mysqli->query($sql) === true){
+    echo "<script>
+        alert('글쓰기 완료');
+        location.href='../../index.php';
+        </script>";
+  } else{
+    echo "Error:".$sql."<br>".$mysqli->error;
+  }
+
 ?>
