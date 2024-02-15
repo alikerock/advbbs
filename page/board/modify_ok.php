7f8f53e859eafbc6f5003602e794b2876c42f901
rock's ezweb <alikerock@gmail.com>
2024-02-15 13:11:36 +0900

    modify_ok complete

--- page/board/modify_ok.php
+++ page/board/modify_ok.php
@@ -1,17 +1,24 @@
 <?php
   include $_SERVER['DOCUMENT_ROOT']."/advbbs/inc/db.php";
+  
+  $bno = $_POST['idx'];
+  $username = $_POST['name'];
+  $userpw = password_hash($_POST['pw'],PASSWORD_DEFAULT);
+  $title = $_POST['title'];
+  $content = $_POST['content'];
+  $date = date('Y-m-d');
 
-  /*
-  변수명 $bno에 idx값 저장
-  $uesername에 새이름 저장
-  $userpw에 새비번을 암호화해서 저장
-  $title에 새제목 저장
-  $content에 새내용 저장
-  $data에 현재 날짜 저장
+  $sql ="UPDATE board set name='{$username}', pw='{$userpw}', title='{$title}', content='{$content}', date='{$date}' where idx = {$bno}";
 
-  변수명 $sql에 board테이블에서 idx가 bno와 일치하는 행의 각 컬럼의 값을 새 값들로 변경
-  $sql 지시문 실행
-  실행결과가 있다면 수정성공 메시지후 홈으로, 없다면 수정실패 메시지 출력후 바로 전 페이지로
-  */
+  if($mysqli->query($sql) === true){
+    echo "<script>
+        alert('글수정 완료');
+        location.href='../../index.php';
+        </script>";
+  } else{
+    echo "<script>
+          history.back();
+        </script>";
+  }
 
 ?>
