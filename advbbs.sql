a9ab9ee4c4549b9d455d0635a2096f673a25961a
rock's ezweb <alikerock@gmail.com>
2024-02-15 10:26:17 +0900

    sql upload

---
+++ advbbs.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 24-02-15 02:25
+-- 서버 버전: 10.4.32-MariaDB
+-- PHP 버전: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `advbbs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `board`
+--
+
+CREATE TABLE `board` (
+  `idx` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `pw` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `date` date NOT NULL,
+  `hit` int(11) DEFAULT NULL,
+  `thumbsup` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 테이블의 덤프 데이터 `board`
+--
+
+INSERT INTO `board` (`idx`, `name`, `pw`, `title`, `content`, `date`, `hit`, `thumbsup`) VALUES
+(1, '홍길동', '1234', '반갑습니다.', '많이 반갑습니다.', '2024-02-14', NULL, NULL),
+(2, '이도령', '1234', '가나다라마바사아자차카파타하', '반갑습니다.', '2024-02-14', NULL, NULL),
+(3, '테스트', '$2y$10$vuX15Hvf6pK980MbVJ61/.vjr3w/3/0DGaVlSzZxdNeYC/gqxpWFS', '테스트 제목', '테스트 내용', '2024-02-15', NULL, NULL);
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `board`
+--
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `board`
+--
+ALTER TABLE `board`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
