d330c638d77abca28587e9f039a8a0bfa481b8ec
rock's ezweb <alikerock@gmail.com>
2024-02-15 11:56:25 +0900

    sql update

--- advbbs.sql
+++ advbbs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-02-15 02:25
+-- 생성 시간: 24-02-15 03:55
 -- 서버 버전: 10.4.32-MariaDB
 -- PHP 버전: 8.2.12
 
@@ -43,9 +43,10 @@ CREATE TABLE `board` (
 --
 
 INSERT INTO `board` (`idx`, `name`, `pw`, `title`, `content`, `date`, `hit`, `thumbsup`) VALUES
-(1, '홍길동', '1234', '반갑습니다.', '많이 반갑습니다.', '2024-02-14', NULL, NULL),
+(1, '홍길동', '1234', '반갑습니다.', '많이 반갑습니다.', '2024-02-14', 4, NULL),
 (2, '이도령', '1234', '가나다라마바사아자차카파타하', '반갑습니다.', '2024-02-14', NULL, NULL),
-(3, '테스트', '$2y$10$vuX15Hvf6pK980MbVJ61/.vjr3w/3/0DGaVlSzZxdNeYC/gqxpWFS', '테스트 제목', '테스트 내용', '2024-02-15', NULL, NULL);
+(3, '테스트', '$2y$10$vuX15Hvf6pK980MbVJ61/.vjr3w/3/0DGaVlSzZxdNeYC/gqxpWFS', '테스트 제목', '테스트 내용', '2024-02-15', NULL, NULL),
+(4, '그린', '$2y$10$P5tfazVCFeBlCeya.iE0yOjDDcRW4Btf.WnRFmg1vZ7cxZ7tUq17e', '그린 테스트', '본문 입니다.\r\n본문 입니다.\r\n본문 입니다.\r\n본문 입니다.', '2024-02-15', 5, NULL);
 
 --
 -- 덤프된 테이블의 인덱스
@@ -65,7 +66,7 @@ ALTER TABLE `board`
 -- 테이블의 AUTO_INCREMENT `board`
 --
 ALTER TABLE `board`
-  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
