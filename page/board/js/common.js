6dd5ae371511e940a47348be09f7bb839e821d84
rock's ezweb <alikerock@gmail.com>
2024-02-19 11:16:01 +0900

    댓글 삭제 script

--- page/board/js/common.js
+++ page/board/js/common.js
@@ -12,5 +12,16 @@ for(let rmb of reply_mod_btn){
     })
   })
 }
+for(let rdb of reply_del_btn){
+  rdb.addEventListener('click',(e)=>{
+    let target = e.target.closest('.reply').querySelector('.del_dialog');
+    let closeBtn = target.querySelector('button:last-of-type');
+    target.setAttribute('open','open');
+    
+    closeBtn.addEventListener('click',()=>{
+      target.removeAttribute('open');
+    })
+  })
+}
 
 //삭제버튼을 클릭하면 할일
